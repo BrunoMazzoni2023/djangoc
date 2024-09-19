1fe91be61d6e241b39abf22f96335e669c39efde
BrunoMazzoni2023 <bruno.bmxdabiciceta@gmail.com>
2024-09-19 02:50:50 -0300

    quarto commit

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vh6_-!$^felpl(0)r95oth+50md@g%xkiv=spg&de)0^-#hs*i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoc.onrender.com']
 
 
 # Application definition
@@ -127,7 +127,7 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
